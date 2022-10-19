d8276bf389dadb4a8205e715f46c3b4789725559
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-10-19 15:42:40 -0700

    Web compatibility dependency installed

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import {Amplify} from 'aws-amplify'
 import awsconfig from './src/aws-exports'
-Amplify.configure(awsconfig)
+Amplify.configure(awsconfig);
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a test!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
